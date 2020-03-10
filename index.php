afaafb82c5b9b0e2403e145ca0a22948573537f6
unknown <franlu.gauto@gmail.com>
2020-03-10 13:44:46 -0300

    primera version

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Carmen Soler</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<div class="bold">La Asunción</div>
+				<div class="bold">Clandestina de</div>
+				<div class="bolder">Carmen Soler.</div>
+			</div>
+			<nav class="menu">
+				<ul>
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Biografía</a></li>
+					<li><a href="#">Sitios Históricos</a></li>
+					<li><a href="#">Análisis de su obra</a></li>
+					<li><a href="#">Obras y manuscritos</a></li>
+					<li><a href="#">Los investigadores</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<div class="video">
+		<video autoplay="autoplay" loop="loop" id="video_background" preload="auto" volume="50"/>
+   			<source src="Memory of a Woman.mp4" type="video/mp4" />
+ 		<video/>
+		<p>Hola</p>
+	</div>
+	<div class="sitios">
+		<div class="sitioscontainer">
+			<p>Sitios Históricos</p>
+			<div class="sitioscarrusel">
+				<img src="img/foto1.jfif">
+				<img src="img/foto2.png">
+				<img src="img/foto3.jfif">
+			</div>
+		</div>
+	</div>
+	<div class="quienfue">
+		<p>¿Quién fue?</p>
+	</div>
+	<div class="legado">
+		<p>Legado</p>
+	</div>
+	<div class="investigadores">
+		<p>Investigadores</p>
+	</div>
+	<footer class="footer">
+		<div class="container2">
+			<div class="firma"><a>Powered by COSO Studio</a></div>
+			<div class="redes">FB</div>
+		</div>
+	</footer>
+</body>
+</html>
