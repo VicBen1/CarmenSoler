f8b9f898ec232de3df1f23fe3502e3f06b2dcb62
VicBen1 <42073636+VicBen1@users.noreply.github.com>
2020-09-14 03:28:01 -0400

    Add files via upload

---
+++ sitios-historicos/sitios-historicos.php
@@ -0,0 +1,337 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no initial-scale=1, maximum-scale=1, minimum-scale=1">
+        <!--CSS-->
+        <link rel="stylesheet" type="text/css" href="estilos2.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://kit.fontawesome.com/8fee2c1115.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Montserrat';
+            }
+        </style>
+    </head>
+    <body>
+    <div class="contenedor">
+            <div class="menures">
+                <input type="checkbox" id="menu-res">
+                <label for="menu-res">
+                <img src="dab.png" alt="" width="30px" height="30px">
+                </label>
+                <nav class="menu2">               
+                    <a href="../index.php">Inicio</a>
+                    <a href="../sobre-su-obra/index.php">Sobre su obra</a>
+                    <a href="../archivo-poemas/paginacion.php">Archivo de Poemas</a>
+                    <a id="current">Sitios de Memoria</a>
+                    <a href="../vida/scrollpage.php">Vida</a>
+                    <a href="../galeria/index.php">Galería</a>
+                </nav>
+            </div>
+            
+            <div class="menu">
+                <a id="../index.php">Inicio</a>
+                <a href="../sobre-su-obra/index.php">Sobre su obra</a>
+                <a href="../archivo-poemas/paginacion.php">Archivo de Poemas</a>
+                <a id="current">Sitios de Memoria</a>
+                <a href="../vida/scrollpage.php">Vida</a>
+                <a href="../galeria/index.php">Galería</a>
+            </div>
+            
+            <div class="cuadro">
+                <h2>La Asuncion<br>clandestina<br>de Carmen</h2>
+                <li class="list">
+                <h4 class="gris"><strong><a class="sub">La&nbsp;</a>Encarnacion.</strong></h4>
+                <h4><strong>San Jeronimo.</strong></h4>
+                <h4><strong>San Vicente.</strong></h4>
+                <h4><strong>Barrio Obrero.</strong></h4>
+                <h4><strong>Ita Pyta Punta.</strong></h4>
+                </li>
+            </div>
+            
+            <div class="articulo">
+                <p class="art1">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 
+                    'Content here, content here', making it look like readable English.
+                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+                    and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                    Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
+                    making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+                    looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+                    and going through the cites of the word in classical literature, discovered the undoubtable source.
+                    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
+                    written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                    </p>          
+            </div>
+            
+            <div class="encar1">
+                
+            </div>
+            
+            <div class="encar2">
+                <h2><strong>La Encarnacion</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <script type="text/javascript" src="repo.js"></script>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="jeronimo1">
+                <h2><strong>San Jeronimo</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="jeronimo2">
+                
+            </div>
+            
+            <div class="vicente1">
+                
+            </div>
+            
+            <div class="vicente2">
+                <h2><strong>San Vicente</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="bo1">
+                <h2><strong>Barrio Obrero</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="bo2">
+                
+            </div>
+            
+            <div class="ipp1">
+                
+            </div>
+            
+            <div class="ipp2">
+                <h2><strong>Ita Pyta Punta</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="bo3">
+                <h2><strong>Barrio Obrero</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="bo4">
+                
+            </div>
+            
+            <div class="ipp3">
+                
+            </div>
+            
+            <div class="ipp4">
+                <h2><strong>Ita Pyta Punta</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="bo5">
+                <h2><strong>Barrio Obrero</strong></h2>
+                <audio id="miAudio" src="mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="bo6">
+                
+            </div>
+            
+            <div class="map" id="mapp">
+                <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMUvykiIxMueCzTi0fdHreus7VqkxkP30&callback=initMap">
+                </script>
+                <script>
+		
+      var map;
+  	 function initMap() {
+        map = new google.maps.Map(document.getElementById('mapp'), {
+		  center: {lat: -25.271471669686484, lng: -57.584015429019935},
+          zoom: 13,
+        });
+        var marker = new google.maps.Marker({
+          position: {lat: -25.271471669686484, lng:-57.584015429019935},
+          map: map,
+	  title: 'ips central'
+        });
+      }
+ 
+      
+	</script>
+                    
+                
+            </div>
+            
+            <div class="footer">
+                
+                <div class="cajita">
+                    <img src="../imgs/muni-11.png">
+                </div>
+                
+                <div class="peque">
+                    <img src="../imgs/lupa.png">
+                    <a href="index.php">Esta investigación</a>
+                </div>
+                
+            </div>
+            
+            
+        </div>
+    </body>
+</html>
