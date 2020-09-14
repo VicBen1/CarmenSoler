221821c6be85c0bb49092c6de8fd31c99014caac
VicBen1 <42073636+VicBen1@users.noreply.github.com>
2020-09-14 03:28:57 -0400

    Add files via upload

---
+++ sitio1/sitios1.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no initial-scale=1, maximum-scale=1, minimum-scale=1">
+        <!--CSS-->
+        <link rel="stylesheet" type="text/css" href="estilos2i.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://kit.fontawesome.com/8fee2c1115.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="owl.carousel.min.css">
+        <link rel="stylesheet" type="text/css" href="owl.theme.default.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
+            <style>
+                body {
+                    font-family: 'Montserrat';
+                }
+            </style>
+    </head>
+    <body>
+    <div class="contenedor">
+            
+            <div class="cabecera">
+                
+                <p class="uno">La Asunción <br>Clandestina de <br><strong>Carmen Soler</strong></p>
+                
+            </div>
+            
+            <div class="menures">
+                <input type="checkbox" id="menu-res">
+                <label for="menu-res">
+                <img src="dab.png" alt="" width="30px" height="30px">
+                </label>
+                <nav class="menu2">
+                
+                
+                
+                    <a href="index.php">Inicio</a>
+                    <a href="index.php">Sitios Históricos</a>
+                    <a href="index.php">Vida</a>
+                    <a href="index.php">Sobre su obra</a>
+                    <a href="index.php">Archivo de Poemas</a>
+                    <a href="index.php">Galería</a>
+                
+            </nav>
+            
+            </div>
+            
+            <div class="menu">
+                    <a href="index.php">Inicio</a>
+                    <a href="index.php" id="current">Sitios Históricos</a>
+                    <a href="index.php">Vida</a>
+                    <a href="index.php">Sobre su obra</a>
+                    <a href="index.php">Archivo de Poemas</a>
+                    <a href="index.php">Galería</a>
+            </div>
+            
+            <div class="encar1">
+                
+            </div>
+            <div class="encar2">
+                <h2><strong>La Encarnacion</strong></h2>
+                <audio id="miAudio" src="menu-pages/mario.mp3"></audio>
+
+                <div id="botones" class="botones2">
+
+                    <button id="iniciar" onclick="iniciar()" class='inic'><i class='fas fa-play'></i></button>
+
+                    <div class="titulos">
+                        <li class="list2">
+                        <p1><strong>Vida Clandestina</strong></p1>
+                        <p2>Escuchar poesia</p2>
+                        </li>
+                    </div>
+
+                </div>
+                <script type="text/javascript" src="menu-pages/repro.js"></script>
+                <p class="psize">Nullam sed venenatis nisi. Quisque posuere leo quis pulvinar ultricies. Nulla lobortis vitae sapien non blandit. 
+                    Suspendisse et ante id sapien consectetur vulputate. Donec vitae massa ipsum. 
+                    Morbi tincidunt vitae purus lacinia cursus. Pellentesque nec feugiat tortor, eu efficitur libero. 
+                    Aliquam rhoncus euismod ligula, ut luctus turpis. Aliquam lobortis tortor tempus, volutpat tellus vel, 
+                    finibus nisi.</p>
+                <a href="sitio1.php">Leer mas...</a>
+            </div>
+            
+            <div class="parrafo">
+                <p>
+        
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                    "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pretium enim, vel luctus magna. Praesent massa lectus, luctus non felis eu, condimentum commodo tellus. Duis elit nisi, bibendum quis eros eget, consectetur mollis velit. Aliquam erat volutpat. Vivamus pharetra magna eu erat luctus gravida. Maecenas bibendum sodales metus, at ornare diam finibus a. Ut odio nisl, elementum quis eros eu, dignissim semper nisi. Nam nulla eros, ultricies quis fringilla sit amet, scelerisque non turpis. Nulla a tortor massa. Maecenas tristique neque in tortor pretium hendrerit. Fusce fringilla risus at leo suscipit pellentesque. Nunc aliquam, magna non hendrerit vehicula, dolor metus aliquam felis, quis rhoncus mi elit a nisl.
+
+                    Etiam vehicula nibh accumsan mauris porttitor posuere. Sed ultricies erat eu lacus blandit dictum. Suspendisse laoreet leo mauris, eu blandit mi consequat in. Curabitur velit nisi, fermentum ac mauris et, volutpat tristique dui. Donec ut orci sagittis, eleifend quam non, tempus sapien. Vestibulum euismod ac urna dignissim malesuada. Aliquam erat volutpat. Integer sollicitudin et odio ut tempus. Fusce sollicitudin mauris a felis lobortis, id tristique justo condimentum. Pellentesque sed lacinia dui. Mauris mi nisl, pharetra non feugiat a, dignissim iaculis mauris. 
+                </p>
+                <p><br>
+                    Ut egestas, sem id mollis molestie, augue turpis varius est, eget consequat dolor mi mattis elit. Maecenas nisl ante, scelerisque sit amet fermentum eget, luctus vel risus. Vivamus viverra augue tellus, sit amet varius lorem lacinia vel. Integer luctus massa sed augue suscipit, rutrum laoreet elit elementum. Nulla ut mollis metus. Vestibulum porta, massa eu vehicula tempus, tortor nisi ultricies justo, nec euismod purus lectus a velit. Quisque ornare, dolor et facilisis luctus, elit tortor cursus quam, nec eleifend lectus eros id sapien. Vestibulum tempor vitae turpis interdum consequat. Vestibulum non ultricies massa, quis dictum magna. Nam tempus erat dui, eget posuere mi lobortis non. Quisque sem metus, interdum quis sodales eget, convallis eget neque. Duis ullamcorper convallis mi, nec vestibulum quam cursus in. Mauris eu sapien mattis, placerat sapien vitae, facilisis sapien. Maecenas lobortis neque eu faucibus posuere. Maecenas eget sodales eros. 
+                </p>
+            </div>
+            
+            <div class="gal1"></div>
+            <div class="gal2"></div>
+            <div class="gal3"></div>
+            <div class="gal4"></div>
+            <div class="gal5"></div>
+            <div class="gal6"></div>
+            
+            <div class="slid wrapper">
+                <style type="text/css">.wrapper{width: 100%; height: auto; overflow: hidden;}</style>
+                <div class="owl-carousel owl-theme">
+                    <div class="item"><h4><img src="imgs/1.jpg" alt="" class="imgsize"></h4></div>
+                    <div class="item"><h4><img src="imgs/2.jpg" alt="" class="imgsize"></h4></div>
+                    <div class="item"><h4><img src="imgs/3.jpg" alt="" class="imgsize"></h4></div>
+                    <div class="item"><h4><img src="imgs/4.jpg" alt="" class="imgsize"></h4></div>
+                    <div class="item"><h4><img src="imgs/5.jpg" alt="" class="imgsize"></h4></div>
+                </div>
+            </div>
+                 <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+                 <script type="text/javascript" src="owl.carousel.min.js"></script>
+                 <script type="text/javascript" src="jquer.js"></script>
+
+            <div class="footer">
+                
+                <div class="cajita">
+                    <img src="imgs/muni-11.png">
+                </div>
+                
+                <div class="peque">
+                    <img src="imgs/lupa.png">
+                    <a href="index.php">Esta investigación</a>
+                </div>
+                
+            </div>
+        </div>
