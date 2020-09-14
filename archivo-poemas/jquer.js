97f66b8ac1a7822a8c7fb9be740b25c4cdd14c41
VicBen1 <42073636+VicBen1@users.noreply.github.com>
2020-09-13 23:49:36 -0400

    Add files via upload

---
+++ archivo-poemas/jquer.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+            dots:false,
+            nav:true
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
